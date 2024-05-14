2382bd3561754b000ec6294c7c7a222e5801f61e
Bartosz Tyczyński <bartosztyczynski@protonmail.com>
2024-05-14 11:41:14 +0200

    Zmieniono widoczność atrybutów klas, wygenerowano getter lda pola total klasy DefaultCountingOutRhymer, dokonano hermetyzacji nieprywatnych atrybutów i usunięto nieużywany setter

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,29 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_NUMS = 12;
+    private static final int MAX_NUMS = 12;
     private final int[] numbers = new int[MAX_NUMS];
 
-    public static final int ERR_CODE = -1;
-    public static final int START_AMOUNT = -1;
-    public int total = START_AMOUNT;
+    private static final int ERR_CODE = -1;
+    private static final int START_AMOUNT = -1;
+
+    public static int getMaxNums() {
+        return MAX_NUMS;
+    }
+
+    public static int getErrCode() {
+        return ERR_CODE;
+    }
+
+    public static int getStartAmount() {
+        return START_AMOUNT;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    private int total = START_AMOUNT;
 
     public void countIn(int in) {
         if (!isFull())
@@ -34,4 +51,7 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int[] getNumbers() {
+        return numbers;
+    }
 }
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int IS_EMPTY = -1;
+    private static final int IS_EMPTY = -1;
     Node last;
     int i;
 
